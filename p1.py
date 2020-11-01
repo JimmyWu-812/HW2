a8f1969406c0e03be01addf7d795a143d4e8fad8
JimmyWu-812 <59006023+JimmyWu-812@users.noreply.github.com>
2020-11-01 14:38:45 +0800

    11/1

--- p1.py
+++ p1.py
@@ -1,19 +1,30 @@
 import numpy as np
 from util import mod_inv
 
-
 def decode(cipher, key):
-    #TODO
-    '''
-    Decode cipher with public key.
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.,?!"
+    S_encrypt = dict()
+    S_decrypt = dict()
+    for i in range(31):
+        S_encrypt[chars[i]] = i
+        S_decrypt[i] = chars[i]
 
-    Arguments:
-        cipher: str, cipher text
-        key: str, public key
+    keyListString = key.split(' ')
+    keyListInt = []
+    for ele in keyListString:
+        keyListInt.append(int(ele))
+    keyMatrix = np.reshape(keyListInt, (3,3))
+    keyMatrix = mod_inv(keyMatrix)
+    
+    cipherListInt = []
+    for char in cipher:
+        cipherListInt.append(S_encrypt[char])
+    cipherMatrix = np.reshape(cipherListInt, (-1,3)).T
+    plainMatrix = np.matmul(keyMatrix, cipherMatrix).T
 
-    Return:
-        plain: str, plain text
-    '''
-
-    plain = ''
+    plain = ""
+    for a in plainMatrix:
+        for b in a:
+            plain += S_decrypt[b % 31]
     return plain
+#decode("C!QER,YNR","25 8 25 9 9 16 28 21 18")
--- p2.py
+++ p2.py
@@ -1,20 +1,27 @@
 import numpy as np
 from util import mod_inv
 
-
 def get_key(cipher, plain):
-    #TODO
-    '''
-    Calculate public key with cipher text and plain text.
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.,?!"
+    S_encrypt = dict()
+    for i in range(31):
+        S_encrypt[chars[i]] = i
+    
+    cipherListInt = []
+    for char in cipher:
+        cipherListInt.append(S_encrypt[char])
+    cipherMatrix = np.reshape(cipherListInt, (-1,3)).T
 
-    Arguments:
-        cipher: str, cipher text
-        plain: str, plain text
-
-    Return:
-        key: str, public key
-    '''
-
-    key = ''
-    return key
-
+    plainListInt = []
+    for char in plain:
+        plainListInt.append(S_encrypt[char])
+    plainMatrix = np.reshape(plainListInt, (-1,3)).T
+    
+    inv = mod_inv(plainMatrix)
+    keyMatrix = np.matmul(cipherMatrix,inv)
+    key = ""
+    for a in keyMatrix:
+        for b in a:
+            key += str(round(b) % 31) + " "
+    return key[:-1]
+#get_key("C!QER,YNR","IS_THAT_W")
